9e1696d8e8383a2a808aaca780b21eb8d9ad76f1
kojirou <kojirouhtc@gmail.com>
2026-04-07 11:36:55 +0800

    upgrade deps

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.3
 
 import PackageDescription
 
 let package = Package(
   name: "BufferUtility",
   platforms: [
-    .macOS(.v10_15)
+    .macOS(.v11),
   ],
   products: [
     .library(name: "BufferUtility", targets: ["BufferUtility"]),
